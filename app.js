b893773548ff844ef68fe19264e7eb705679c0c5
lDantonl <danton5276@gmail.com>
2013-05-11 09:56:38 -0400

    Major commit, set up server to serve .ejs files. Also, route handling is in place and a general file structure has been created. Time for innovation and progress!

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-// Node Module dependencies:
+// Node dependencies:
 var express = require('express');
 var http = require('http');
 var path = require('path');
-
-var app = express.createServer(express.logger());
-var port = process.env.PORT || 1337;
-
+// Application requirements //
+var home = require('./routes/home');
 
 
+app = express(); 
 
-
+app.configure(
+	function()
+	{
+		app.set('port', process.env.PORT || 1337);
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'ejs');
+		app.use(express.favicon(__dirname + "/public/images/favicon.ico"));
+		app.use(express.logger('dev'));
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(express.cookieParser('your secret here'));
+		app.use(express.static(path.join(__dirname, 'public')));
+	}
+);
 
 
 // Error Handler
-app.configure('development', 
+app.configure('developement',
 	function()
 	{
 		app.use(express.errorHandler());
 	}
 );
 
-// Server Initilization Logging
-app.listen(port, 
+// Server Logging
+app.listen(app.get('port'), 
 	function() 
 	{
-		console.log("Express server listening on port " + port);
+		console.log("Express server listening on port " + app.get('port'));
 	}
 );
 
 // Defines routes, and thier handlers
-app.get('/', 
-	function(request, response) 
-	{
-		response.send('Hello World!');
-	}
-);
+app.get('/', home.home);
---
+++ routes/home.js
@@ -0,0 +1,7 @@
+
+
+exports.home = function(req, res)
+{
+	console.log('serving home');
+	res.render('home', {title: 'Welcome To Derek Anton\'s page'});
+};
