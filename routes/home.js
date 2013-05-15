75cc846409b09f671690f1b13e14a6a075c37983
lDantonl <danton5276@gmail.com>
2013-05-16 00:24:27 +0100

    added functionality to the header.ejs template to recognize which page is being served. this is denoted by a JSON object passes by the render call.

--- routes/home.js
+++ routes/home.js
@@ -2,5 +2,5 @@
 exports.home = function(req, res)
 {
 	console.log('serving home');
-	res.render('home', {title: 'Welcome To Derek Anton\'s page'});
+	res.render('home', {title: 'Welcome To Derek Anton\'s page', currPage: 'Home'});
 };
